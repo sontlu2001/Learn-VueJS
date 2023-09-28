7eb4be2406fde9783b47bec46e5d462034637e7f
Do Son <sonnvc2001@gmail.com>
2023-09-28 15:15:00 +0700

    Method Event Handlers

--- main.js
+++ main.js
@@ -1,30 +1,13 @@
 new Vue({
   el: "#app",
   data: {
-    count: 0,
-    counter: null,
-    elementStop: "<button>stop</button>",
-    elementReset: "<button>reset</button>",
-    isActive: false,
+    x: 0,
+    y: 0,
   },
   methods: {
-    getCount: function () {
-      return this.count;
-    },
-    start: function () {
-      if (this.count) return;
-      this.counter = setInterval(() => {
-        this.count++;
-      }, 1000);
-      this.isActive = true;
-    },
-    stop: function () {
-      clearInterval(this.counter);
-    },
-    reset: function () {
-      this.stop();
-      this.count = 0;
-      this.isActive = false;
+    updateCoordinates: function (event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
     },
   },
 });
