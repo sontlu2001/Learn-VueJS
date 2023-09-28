3d5fb611d1f36ae36c13d7f7523330dd1dd633a2
Do Son <sonnvc2001@gmail.com>
2023-09-28 13:45:26 +0700

    interpolation_attributes

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ new Vue({
   el: "#app",
   data: {
     count: 0,
+    counter: null,
   },
   methods: {
     getCount: function () {
       return this.count;
     },
-    increment: function () {
-      this.count++;
+    start: function () {
+      if (this.count) return;
+      this.counter = setInterval(() => {
+        this.count++;
+      }, 1000);
+    },
+    stop: function () {
+      clearInterval(this.counter);
+    },
+    isActive: function () {
+      return this.count >= 10 ? true : false;
     },
   },
 });
