57116441709dfba22d139623e18e8647e5b740b7
Do Son <sonnvc2001@gmail.com>
2023-09-28 14:25:37 +0700

    Raw HTML

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ new Vue({
   data: {
     count: 0,
     counter: null,
+    elementStop: "<button>stop</button>",
+    elementReset: "<button>reset</button>",
+    isActive: false,
   },
   methods: {
     getCount: function () {
@@ -13,12 +16,15 @@ new Vue({
       this.counter = setInterval(() => {
         this.count++;
       }, 1000);
+      this.isActive = true;
     },
     stop: function () {
       clearInterval(this.counter);
     },
-    isActive: function () {
-      return this.count >= 10 ? true : false;
+    reset: function () {
+      this.stop();
+      this.count = 0;
+      this.isActive = false;
     },
   },
 });
