4af3b9c71f1edb6d36506d74061d329a761a5987
Do Son <sonnvc2001@gmail.com>
2023-09-29 15:52:13 +0700

    Computed

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 new Vue({
   el: "#app",
-  data: {},
-  methods: {},
+  data: {
+    a: 0,
+    b: 0,
+  },
+  computed: {
+    getValueA() {
+      console.log("getValueA");
+      return this.a;
+    },
+    getValueB() {
+      console.log("getValueB");
+      return this.b;
+    },
+  },
+  methods: {
+    // getValueA() {
+    //   console.log("getValueA");
+    //   return this.a;
+    // },
+    // getValueB() {
+    //   console.log("getValueB");
+    //   return this.b;
+    // },
+  },
 });
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Computed Properties
+
+`Computed Properties` được sử dụng để tính toán và theo dõi giá trị dựa trên các thuộc tính dữ liệu khác trong Vue instance. Kết quả của Computed Properties được lưu trữ và caching tự động, giúp tối ưu hóa hiệu suất.
