70b8848313779dda37cba9660901233c94516835
Do Son <sonnvc2001@gmail.com>
2023-09-29 15:46:04 +0700

    Event Handling

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 new Vue({
   el: "#app",
-  data: {},
-  methods: {},
+  data: {
+    firstName: "",
+    lastName: "",
+    gender: true,
+  },
+  methods: {
+    changeValueInput: function (event) {
+      if (event.target.name === "gender") {
+        this.gender = event.target.checked;
+      }
+      this[event.target.name] = event.target.value;
+    },
+    submit: function () {
+      console.log(this.firstName, this.lastName, this.gender);
+    },
+  },
 });
